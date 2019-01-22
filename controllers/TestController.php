9137b998b850f0c2beebe54e408245eab64e3206
Mr.L@mbert_13 <Lam_13@mail.ru>
2019-01-22 23:04:13 +0300

    Lesson 4 ex 4 done

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -34,4 +34,20 @@ class TestController extends Controller
             'err' => VarDumper::dumpAsString($model->getErrors(), 5, true),
         ]);*/
     }
+
+    public function actionInsert() {
+        \Yii::$app->db->createCommand()->batchInsert(
+            'user',
+            ['username', 'password_hash', 'auth_key', 'creator_id', 'updater_id', 'created_at', 'updated_at'],
+            [
+                ['user_1', 'hash_1', 'auth_1', 11, 111, time(), time()],
+                ['user_2', 'hash_2', 'auth_2', 22, 222, time(), time()],
+                ['user_3', 'hash_3', 'auth_3', 33, 333, time(), time()],
+            ]
+        );
+    }
+
+    public function actionSelect() {
+
+    }
 }
