65ab6a567640b4120051ef378152a6d38193f8c4
Mr.L@mbert_13 <Lam_13@mail.ru>
2019-01-22 23:49:10 +0300

    Lesson 4 main completed

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -3,6 +3,7 @@
 namespace app\controllers;
 
 use app\models\Product;
+use yii\db\Expression;
 use yii\db\Query;
 use yii\helpers\VarDumper;
 use yii\web\Controller;
@@ -15,39 +16,65 @@ class TestController extends Controller
      * @return string
      */
     public function actionIndex() {
-        $model = new Product(
-            ['id' => 13, 'name' => '  <br/>  Name', 'price' => 135, 'created_at' => 132]
-        );
 
-        $query = new Query();
-        $result = $query->from('product')
-            ->where(['id' => [1, 2, 4]])
-            ->orderBy(['id' => SORT_DESC])
-            ->all();
-        _end($result);
-
-        $model->validate();
-        /*return $this->render('index', [
-            'testmodel' => \Yii::$app->test->getProperty(),
-            'validate' => VarDumper::dumpAsString($model->validate(), 3, true),
-            'lesson' => VarDumper::dumpAsString($model->safeAttributes(), 5, true),
-            'err' => VarDumper::dumpAsString($model->getErrors(), 5, true),
-        ]);*/
+        return $this->render('index');
     }
 
     public function actionInsert() {
-        \Yii::$app->db->createCommand()->batchInsert(
+        /**
+         * 3) В экшене insert TestController-а через Yii::$app->db->createCommand()->insert()
+         * вставить несколько записей в таблицу user, в поле password_hash можно вставить
+         * произвольные значения, поле id заполняется автоматически.
+         */
+        $rows = \Yii::$app->db->createCommand()->batchInsert(
             'user',
             ['username', 'password_hash', 'auth_key', 'creator_id', 'updater_id', 'created_at', 'updated_at'],
             [
-                ['user_1', 'hash_1', 'auth_1', 11, 111, time(), time()],
-                ['user_2', 'hash_2', 'auth_2', 22, 222, time(), time()],
-                ['user_3', 'hash_3', 'auth_3', 33, 333, time(), time()],
+                ['user_7', 'hash_7', 'auth_7', 77, 777, time(), time()],
+                ['user_8', 'hash_8', 'auth_8', 88, 888, time(), time()],
+                ['user_9', 'hash_9', 'auth_9', 99, 999, time(), time()],
             ]
-        );
+        )->execute();
+        return $this->renderContent(($rows > 0) ? 'Data was added' : 'Error');
+
     }
 
     public function actionSelect() {
+        /**
+         * Используя \yii\db\Query в экшене select TestController выбрать из user:
+         * а) Запись с id=1
+         */
+        $query_a = new Query();
+        $result_a = $query_a
+            ->from('user')
+            ->where(['id' => 1])
+            ->one();
 
+        /**
+         * б) Все записи с id>1 отсортированные по имени (orderBy())
+         */
+        $query_b = new Query();
+        $result_b = $query_b
+            ->from('user')
+            ->where(['>', 'id', 1])
+            ->orderBy('username', SORT_ASC)
+            ->all();
+
+        /**
+         * в) количество записей.
+         */
+        $query_c = new Query();
+        $result_c = $query_c
+            ->from('user')
+            ->count();
+
+        //Вывод результата
+        $result = VarDumper::dumpAsString($result_a, 5, true)
+            . '<hr>' . VarDumper::dumpAsString($result_b, 5, true)
+            . '<hr>' . 'Количество записей всего:'
+            . VarDumper::dumpAsString($result_c, 5, true);
+
+        return $this->renderContent($result);
     }
+
 }
--- views/test/index.php
+++ views/test/index.php
@@ -1,33 +1,11 @@
 <?php
-/**
- * @var $model \app\models\TestModel
- * @var $prod \app\models\Product
- * @var $testmodel \app\components\TestService
- * @var $lesson string
- */
+
+use yii\helpers\Html;
+
 ?>
 
-<?php if (isset($model)): ?>
-  <h3>Информация:</h3>
-  <p>Имя: <u><?= $model->name ?></u></p>
-  <p>Дата рождения: <u><?= $model->birthday ?> г.</u></p>
-  <p>Город: <u><?= $model->city ?></u></p>
-  <p>О себе: <u><?= $model->about ?></u></p>
-  <hr>
-<?php endif; ?>
-
-<?php if (isset($prod)): ?>
-  <?= \yii\widgets\DetailView::widget(['model' => $prod]) ?>
-  <hr>
-<?php endif; ?>
-
-Данные из метода getProperty() компонента test: <br>
-<b><?= $testmodel ?></b>
-<hr>
-Данные урока(для повторения без использования <b>return</b>): <br>
-<p>Validate: <?= $validate ?></p>
-<p>GetErrors:</p>
-<?= $err ?>
-<p>GetAttributes:</p>
-<?= $lesson ?>
+<ul>
+  <li><?= Html::a('INSERT', ['test/insert']) ?></li>
+  <li><?= Html::a('SELECT', ['test/select']) ?></li>
+</ul>
 
