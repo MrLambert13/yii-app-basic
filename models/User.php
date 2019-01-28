32b3631fdab5ee92406079267275b2cb7118bf3e
Mr.L@mbert_13 <Lam_13@mail.ru>
2019-01-28 22:28:23 +0300

    lesson 6

--- models/User.php
+++ models/User.php
@@ -19,7 +19,7 @@ namespace app\models;
  * @property Task[]     $accessedTasks
  * @property TaskUser[] $taskUsers
  */
-class User extends \yii\db\ActiveRecord
+class User extends \yii\db\ActiveRecord implements \yii\web\IdentityInterface
 {
     const RELATION_CREATED_TASKS = 'createdTasks';
     const RELATION_UPDATED_TASKS = 'updatedTasks';
@@ -60,6 +60,51 @@ class User extends \yii\db\ActiveRecord
             'created_at' => 'Created At',
             'updated_at' => 'Updated At',
         ];
+    }
+
+    /**
+     * Finds an identity by the given ID.
+     *
+     * @param string|int $id the ID to be looked for
+     *
+     * @return IdentityInterface|null the identity object that matches the given ID.
+     */
+    public static function findIdentity($id) {
+        return static::findOne($id);
+    }
+
+    /**
+     * Finds an identity by the given token.
+     *
+     * @param string $token the token to be looked for
+     *
+     * @return IdentityInterface|null the identity object that matches the given token.
+     */
+    public static function findIdentityByAccessToken($token, $type = null) {
+        return static::findOne(['access_token' => $token]);
+    }
+
+    /**
+     * @return int|string current user ID
+     */
+    public function getId() {
+        return $this->id;
+    }
+
+    /**
+     * @return string current user auth key
+     */
+    public function getAuthKey() {
+        return $this->auth_key;
+    }
+
+    /**
+     * @param string $authKey
+     *
+     * @return bool if auth key is valid for current user
+     */
+    public function validateAuthKey($authKey) {
+        return $this->getAuthKey() === $authKey;
     }
 
     /**
