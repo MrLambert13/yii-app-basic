fc45923bcc179fae20605a2300e0d3291444cf03
Mr.L@mbert_13 <Lam_13@mail.ru>
2019-02-03 17:48:17 +0300

    lesson 6 correct access on task page

--- controllers/TaskController.php
+++ controllers/TaskController.php
@@ -8,6 +8,7 @@ use app\models\Task;
 use yii\behaviors\BlameableBehavior;
 use yii\behaviors\TimestampBehavior;
 use yii\data\ActiveDataProvider;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -26,6 +27,15 @@ class TaskController extends Controller
                 'class' => VerbFilter::className(),
                 'actions' => [
                     'delete' => ['POST'],
+                ],
+            ],
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
                 ],
             ],
         ];
